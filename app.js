2e3c915aea31188106e757ed45b5477b97b35b9a
poom <poom>
2026-04-06 14:14:03 +0700

    fix bug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const MemoryStore = require('memorystore')(session);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/public', express.static(path.join(__dirname, 'public')));
 // =========================================================
 // 🗄️ 1. DATABASE CONNECTION
 // =========================================================
